932a1b0c377cc8074d6e2fc7feeb9ab6103e23c9
Ryan Soklaski <rsoklaski@gmail.com>
2022-09-17 11:44:34 -0400

    add alphabet and words

---
+++ src/phantom_tensors/alphabet.py
@@ -0,0 +1,63 @@
+"""Convenient definitions of 
+
+<Letter> = NewType('<Letter>', int) 
+
+to be used as descriptive axis labels in shape types"""
+
+from typing import NewType as _NewType
+
+__all__ = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+
+A = _NewType("A", int)
+B = _NewType("B", int)
+C = _NewType("C", int)
+D = _NewType("D", int)
+E = _NewType("E", int)
+F = _NewType("F", int)
+G = _NewType("G", int)
+H = _NewType("H", int)
+I = _NewType("I", int)
+J = _NewType("J", int)
+K = _NewType("K", int)
+L = _NewType("L", int)
+M = _NewType("M", int)
+N = _NewType("N", int)
+O = _NewType("O", int)
+P = _NewType("P", int)
+Q = _NewType("Q", int)
+R = _NewType("R", int)
+S = _NewType("S", int)
+T = _NewType("T", int)
+U = _NewType("U", int)
+V = _NewType("V", int)
+W = _NewType("W", int)
+X = _NewType("X", int)
+Y = _NewType("Y", int)
+Z = _NewType("Z", int)
---
+++ src/phantom_tensors/words.py
@@ -0,0 +1,61 @@
+"""Convenient definitions of 
+
+<Word> = NewType('<Word>', int) 
+
+to be used as descriptive axis names in shape types"""
+from typing import NewType as _NewType
+
+__all__ = [
+    "Axis",
+    "Batch",
+    "Channel",
+    "Embed",
+    "Filter",
+    "Height",
+    "Kernel",
+    "Logits",
+    "Score",
+    "Time",
+    "Vocab",
+    "Width",
+    "Axis1",
+    "Axis2",
+    "Axis3",
+    "Axis4",
+    "Axis5",
+    "Axis6",
+    "Dim1",
+    "Dim2",
+    "Dim3",
+    "Dim4",
+    "Dim5",
+    "Dim6",
+]
+
+Axis = _NewType("Axis", int)
+Batch = _NewType("Batch", int)
+Channel = _NewType("Channel", int)
+Embed = _NewType("Embed", int)
+Filter = _NewType("Filter", int)
+Height = _NewType("Height", int)
+Kernel = _NewType("Kernel", int)
+Logits = _NewType("Logits", int)
+Score = _NewType("Score", int)
+Time = _NewType("Time", int)
+Vocab = _NewType("Vocab", int)
+Width = _NewType("Width", int)
+
+Axis1 = _NewType("Axis1", int)
+Axis2 = _NewType("Axis2", int)
+Axis3 = _NewType("Axis3", int)
+Axis4 = _NewType("Axis4", int)
+Axis5 = _NewType("Axis5", int)
+Axis6 = _NewType("Axis6", int)
+
+Dim1 = _NewType("Dim1", int)
+Dim2 = _NewType("Dim2", int)
+Dim3 = _NewType("Dim3", int)
+Dim4 = _NewType("Dim4", int)
+Dim5 = _NewType("Dim5", int)
+Dim6 = _NewType("Dim6", int)
+
