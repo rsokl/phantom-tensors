3186ec8153d6a41c6e6c0cd442fdd9689971939c
Ryan Soklaski <rsoklaski@gmail.com>
2022-09-18 22:52:32 -0400

    fix tests

--- tests/test_type_properties.py
+++ tests/test_type_properties.py
@@ -8,12 +8,16 @@ from hypothesis import assume, given
 from typing_extensions import Literal
 
 
-@pytest.mark.xfail(sys.version_info < (3, 9))
+@pytest.mark.xfail(
+    sys.version_info < (3, 9), reason="Tuple normalization introduced in 3.9"
+)
 def test_literal_singlet_tuple_same_as_scalar():
     assert Literal[(1,)] is Literal[1]  # type: ignore
 
 
-@pytest.mark.xfail(sys.version_info < (3, 8))
+@pytest.mark.xfail(
+    sys.version_info < (3, 8), reason="Literal identity introduced in 3.8"
+)
 @given(...)
 def test_literals_can_check_by_identity(a: Tuple[int, ...], b: Tuple[int, ...]):
     assume(len(a))
@@ -22,3 +26,8 @@ def test_literals_can_check_by_identity(a: Tuple[int, ...], b: Tuple[int, ...]):
     assert Literal[a] is Literal[a]  # type: ignore
     assert Literal[b] is Literal[b]  # type: ignore
     assert Literal[a] is not Literal[b]  # type: ignore
+
+
+def test_literal_hashes_consistently():
+    assert {Literal[1]: 1}[Literal[1]] == 1
+    assert hash(Literal[1, 2]) == hash(Literal[1, 2])
