2911038f47d5dbbab6d68b0d728b7f18c8c12fbc
Ryan Soklaski <rsoklaski@gmail.com>
2022-09-18 22:47:58 -0400

    fix tests

--- tests/test_type_properties.py
+++ tests/test_type_properties.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
+import sys
 from typing import Tuple
 
+import pytest
 from hypothesis import assume, given
 from typing_extensions import Literal
 
 
+@pytest.mark.xfail(sys.version_info < (3, 9))
 def test_literal_singlet_tuple_same_as_scalar():
     assert Literal[(1,)] is Literal[1]  # type: ignore
 
 
+@pytest.mark.xfail(sys.version_info < (3, 8))
 @given(...)
 def test_literals_can_check_by_identity(a: Tuple[int, ...], b: Tuple[int, ...]):
     assume(len(a))
