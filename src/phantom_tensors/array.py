1566c1d64fe27508105bced58098116ff5a35583
Ryan Soklaski <ry26099@mit.edu>
2022-09-20 20:56:12 -0400

    Make SupportsArray a standard generic

--- src/phantom_tensors/array.py
+++ src/phantom_tensors/array.py
@@ -1,12 +1,8 @@
 # pyright: strict
-from typing import TYPE_CHECKING, Any, Tuple
+from typing import Any, Tuple
 
 import typing_extensions as _te
 from typing_extensions import Protocol, runtime_checkable
-
-from phantom_tensors._internals import check
-
-from ._internals import check
 
 __all__ = ["SupportsArray"]
 
@@ -16,31 +12,6 @@ Shape = _te.TypeVarTuple("Shape")
 
 @runtime_checkable
 class SupportsArray(Protocol[_te.Unpack[Shape]]):
-
-    if not TYPE_CHECKING:
-        _cache = {}
-
-        @classmethod
-        def __class_getitem__(cls, key):
-            if not isinstance(key, tuple):
-                key = (key,)
-
-            class PhantomHasShape:
-                __bound__ = (object,)
-                __args__ = key
-
-                def __array__(self) -> Any:
-                    ...
-
-                def shape(self) -> Tuple[int, ...]:
-                    ...
-
-                @classmethod
-                def __instancecheck__(cls, __instance: Any) -> bool:
-                    return check(key, __instance.shape)
-
-            return PhantomHasShape
-
     def __array__(self) -> Any:
         ...
 
--- tests/arrlike.py
+++ tests/arrlike.py
@@ -1,10 +1,17 @@
-from typing import Tuple
+from typing import Any, Tuple
 
 
 class ImplementsArray:
     def __init__(self, shape: Tuple[int, ...]) -> None:
         assert all(i >= 0 and isinstance(i, int) for i in shape)
-        self.shape = shape
+        self._shape = shape
+
+    @property
+    def shape(self) -> Tuple[int, ...]:
+        return self._shape
+
+    def __array__(self) -> Any:
+        ...
 
 
 def arr(*shape: int) -> ImplementsArray:
